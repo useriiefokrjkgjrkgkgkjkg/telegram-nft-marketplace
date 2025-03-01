a82627cb4a388920e82c9f970aac3b81a7b40a4f
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-01 21:54:06 +0300

    Add Telegram Web App script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { ChakraProvider } from '@chakra-ui/react';
 import type { ReactNode } from 'react';
+import Script from 'next/script';
 
 export default function RootLayout({
   children,
@@ -10,6 +11,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
+      <head>
+        <Script 
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
+      </head>
       <body>
         <ChakraProvider>
           {children}
