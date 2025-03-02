011046f8107d428be3164f8be6b74c74ef9b00ac
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-02 21:33:47 +0300

    Обновлена ссылка на логотип TON

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ declare global {
 // Компонент для логотипа TON
 const TonLogo = ({ size = "24px" }: { size?: string }) => (
   <Image 
-    src="https://ton.org/assets/ton_symbol.svg"
+    src="https://ton.org/download/ton_symbol.svg"
     alt="TON" 
     width={size} 
     height={size}
