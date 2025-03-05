90e9dc5d4665fe17b3d20e2f41c574a0409d9dcb
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-05 18:22:31 +0300

    Исправлены типы для Telegram WebApp

--- app/page.tsx
+++ app/page.tsx
@@ -30,10 +30,10 @@ import StartCommand from './components/StartCommand';
 declare global {
   interface Window {
     Telegram?: {
-      WebApp: {
-        expand: () => void;
-        enableClosingConfirmation: () => void;
-        close: () => void;
+      WebApp?: {
+        expand?: () => void;
+        enableClosingConfirmation?: () => void;
+        close?: () => void;
         initDataUnsafe?: {
           user?: {
             photo_url?: string;
