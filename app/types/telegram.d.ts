41f17e0edd1b993eb29d39cea224d4d24475d5e5
ProGramiiSt <ps5plus2608@gmail.com>
2025-03-05 18:30:14 +0300

    Исправлено объявление типа Telegram в Window

--- app/types/telegram.d.ts
+++ app/types/telegram.d.ts
@@ -11,17 +11,8 @@ interface TelegramWebApp {
 
 declare global {
   interface Window {
-    Telegram: {
-      WebApp: {
-        expand: () => void;
-        enableClosingConfirmation: () => void;
-        close: () => void;
-        initDataUnsafe?: {
-          user?: {
-            photo_url?: string;
-          };
-        };
-      };
+    Telegram?: {
+      WebApp: TelegramWebApp;
     };
   }
 }
